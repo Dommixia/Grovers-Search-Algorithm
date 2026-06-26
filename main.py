537e7bdbb70e4ae5bd9df7de90d9b38257668cdb
Dommixia <dommixia@gmail.com>
2026-06-26 13:24:08 +0530

    Code Readability

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ import time
 
 num_qubits = 10
 N = 2**num_qubits
-
 dataset = np.random.uniform(1,100, N)
 print(f"Dataset Size: {N}")
-
 target_value = 155
 target_index = np.random.randint(N)
 dataset[target_index] = target_value
-
 print(f"Target Value: {target_value} - Target Index: {target_index}")
-
+print("--------------------------------------------------")
 #CLASSICAL SEARCH
+print("Classical Search")
 found_index = -1
 c_start = time.perf_counter()
-
 for i in range(len(dataset)):
     if dataset[i] == target_value:
         found_index = i
@@ -26,3 +23,4 @@ for i in range(len(dataset)):
 c_end= time.perf_counter()
 c_time = c_end - c_start
 print(f"Time taken: {c_time}")
+print("--------------------------------------------------")
